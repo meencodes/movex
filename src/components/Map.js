1bf9371dc155ae9ac5cae651c0dcf5835487bcf6
majasmee <ameen_aic@hotmail.com>
2019-12-25 13:44:15 +0700

    Map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
     GoogleMap,
     withGoogleMap,
-    withScriptjs
+    withScriptjs,
+    Marker
 } from 'react-google-maps'
+import axios from 'axios';
 
-const Map = (props) => {
-    return (
-        <GoogleMap
-            defaultZoom={10}
-            defaultCenter={{ lat: -34.397, lng: 150.644 }}
-        />
+class Map extends Component {
+    constructor(props) {
+        super(props);
 
-    );
+        this.state = {
+            data: []
+        }
+    }
+
+    pushDataFromCryptomap(){
+        axios.get(`https://api.cryptomap.org/api/stores/on_map/pearl`).then((res) => {
+    
+            let obj = res.data.store_data
+            // console.log(obj);
+            this.setState({ data: obj })
+        })
+    }
+
+    componentDidMount() {
+        this.pushDataFromCryptomap();
+    }
+    render() {
+        return (
+            <GoogleMap
+                defaultZoom={10}
+                defaultCenter={{ lat: 13.7112531, lng: 100.6536093 }}
+            > 
+
+                {this.state.data.map(item => (
+                    <Marker 
+                        key={item.category}
+                        position={{
+                            lat: parseFloat(item.lat),
+                            lng: parseFloat(item.lon)
+                        }}
+                    />
+    
+                ))}
+
+            </GoogleMap>
+        )
+    }
 }
 
 export default withScriptjs(
